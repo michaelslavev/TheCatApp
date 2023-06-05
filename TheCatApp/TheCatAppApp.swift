695b3a95f5ab9a386a7240c4b5e12b4e87bf94a2
Michael Slavev <michaelslavev@gmail.com>
2023-06-05 23:21:52 +0200

    Initial Commit

---
+++ TheCatApp/TheCatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCatAppApp.swift
+//  TheCatApp
+//
+//  Created by Michael Slavev on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TheCatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
